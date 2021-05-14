bf3af6e190a9578b6f5d12c2751e06b59c0f0e38
Coy McNew <mcnewcp@gmail.com>
2021-05-14 12:11:23 -0500

    changed inputs to current scenario

--- 00-non-shiny-for-development/01-UI_load.R
+++ 00-non-shiny-for-development/01-UI_load.R
@@ -3,21 +3,21 @@
 #Original Mortgage Principal
 P_orig <- 625000
 #Original Interest Rate
-r_a_orig <- 4.0
+r_a_orig <- 3.99
 #Mortgage Length: "15 year" or "30 year"
 n_orig <- "30 year"
 #Mortgage Start Date
-t0_orig <- ymd("2019-07-01")
+t0_orig <- ymd("2019-08-01")
 
 #Refinance Interest Rate
-r_a_refi <-  3.3
+r_a_refi <-  2.59
 #Refinance Length: "15 year" or "30 year"
-n_refi <- "30 year" 
+n_refi <- "15 year" 
 #Refinance Start Date
-t0_refi <- ymd("2021-05-01")
+t0_refi <- ymd("2021-06-01")
 #Closing Costs
-close_cost <- 5000
+close_cost <- 3200
 #Points Y/N
-points_yn <- "Yes"
+points_yn <- "No"
 #Points
 points <- 3
